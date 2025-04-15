e23d220adfeac752c60cd89d146d68cd55385759
Jason <jbjbro6@gmail.com>
2025-04-14 22:38:58 -0400

    Removed the background for the loading pages

--- src/app/players/loading.tsx
+++ src/app/players/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[#1A3E2A]">
+    <div className="min-h-screen flex items-center justify-center">
       <div className="text-[#9A9540]">Loading Players...</div>
     </div>
   );
