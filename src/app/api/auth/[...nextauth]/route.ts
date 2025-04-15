256b3ff5de66df2ac8541400a99293ad311a5f36
Jason <jbjbro6@gmail.com>
2025-04-14 22:29:35 -0400

    Have admin session token only last 15 minutes before expiring

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -64,7 +64,7 @@ const handler = NextAuth({
   },
   session: {
     strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60, // 30 days
+    maxAge: 30 * 30, // 15 Minutes  
   },
   callbacks: {
     async jwt({ token, user }) {
