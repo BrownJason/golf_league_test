8fa7e4a9556a28841588befb073c4599c812a412
Jason <jbjbro6@gmail.com>
2025-04-17 20:42:27 -0400

    Remove redis as the free version is to llittle for the amount of api calls

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,6 @@
 import { Player } from '@/app/admin/players/columns';
 import { WeeklyScore } from '@/app/weekly_score/score-columns';
 import { WeeklyWinnings } from '@/app/weekly_score/winnings-columns';
-import client from './redis';
 
 function getApiUrl(path: string): string {
   // For client-side requests
@@ -21,10 +20,6 @@ function getApiUrl(path: string): string {
 export async function fetchPlayers(): Promise<Player[]> {
   try {
     const url = getApiUrl('/api/players');
-    const cachedData = await client.get('players');
-    if (cachedData) {
-      return JSON.parse(cachedData);
-    }
     const response = await fetch(url, {
       method: 'GET',
       next: { revalidate: 0 },
@@ -36,7 +31,6 @@ export async function fetchPlayers(): Promise<Player[]> {
     }
 
     const data = await response.json();
-    await client.set('players', JSON.stringify(data), { EX: 300 });
     return Array.isArray(data) ? data : [];
   } catch (error) {
     console.error('Fetch Error:', error);
@@ -47,10 +41,6 @@ export async function fetchPlayers(): Promise<Player[]> {
 export async function fetchWeeklyScores(): Promise<WeeklyScore[]> {
   try {
     const url = getApiUrl('/api/weekly-scores');
-    const cachedData = await client.get('weekly-scores');
-    if (cachedData) {
-      return JSON.parse(cachedData);
-    }
     const response = await fetch(url, {
       method: 'GET',
       next: { revalidate: 0 },
@@ -62,7 +52,6 @@ export async function fetchWeeklyScores(): Promise<WeeklyScore[]> {
     }
 
     const data = await response.json();
-    await client.set('weekly-scores', JSON.stringify(data), { EX: 300 });
     return Array.isArray(data) ? data : [];
   } catch (error) {
     console.error('Fetch Error:', error);
@@ -72,10 +61,6 @@ export async function fetchWeeklyScores(): Promise<WeeklyScore[]> {
 
 export async function fetchWeeklyWinnings(): Promise<WeeklyWinnings[]> {
   const url = getApiUrl('/api/weekly-winnings');
-  const cachedData = await client.get('weekly-winnings');
-  if (cachedData) {
-    return JSON.parse(cachedData);
-  }
   const response = await fetch(url, {
     cache: 'no-store',
     next: { revalidate: 0 }
@@ -84,16 +69,11 @@ export async function fetchWeeklyWinnings(): Promise<WeeklyWinnings[]> {
     throw new Error('Failed to fetch weekly winnings');
   }
   const data = await response.json();
-  await client.set('weekly-winnings', JSON.stringify(data), { EX: 300 } );
   return Array.isArray(data) ? data : [];
 }
 
 export async function fetchPlayer(playerId: number): Promise<Player> {
   const url = getApiUrl(`/api/players/${playerId}`);
-  const cachedData = await client.get(`player-${playerId}`);
-  if (cachedData) {
-    return JSON.parse(cachedData);
-  }
   const response = await fetch(url, {
     next: { revalidate: 0 },
     cache: 'no-store'
@@ -102,16 +82,11 @@ export async function fetchPlayer(playerId: number): Promise<Player> {
     throw new Error('Failed to fetch player');
   }
   const data = await response.json();
-  await client.set(`player-${playerId}`, JSON.stringify(data), { EX: 300 });
   return data;
 }
 
 export async function fetchPlayerScores(playerId: number): Promise<WeeklyScore[]> {
   const url = getApiUrl(`/api/players/${playerId}/scores`);
-  const cachedData = await client.get(`player-scores-${playerId}`);
-  if (cachedData) {
-    return JSON.parse(cachedData);
-  }
   const response = await fetch(url, {
     next: { revalidate: 0 },
     cache: 'no-store'
@@ -120,18 +95,12 @@ export async function fetchPlayerScores(playerId: number): Promise<WeeklyScore[]
     throw new Error('Failed to fetch player scores');
   }
   const data = await response.json();
-  await client.set(`player-scores-${playerId}`, JSON.stringify(data), { EX: 300 });
   return data;
 }
 
 export async function fetchPlayerScoresByWeek(playerId: number, weekDate: string): Promise<WeeklyScore[]> {
   const week = encodeURIComponent(weekDate);
   const url = getApiUrl(`/api/players/${playerId}/scores?week=${week}`);
-
-  const cachedData = await client.get(`player-scores-${playerId}-${week}`);
-  if (cachedData) {
-    return JSON.parse(cachedData);
-  }
   const response = await fetch(url, {
     next: { revalidate: 0 },
     cache: 'no-store'
@@ -140,17 +109,12 @@ export async function fetchPlayerScoresByWeek(playerId: number, weekDate: string
     throw new Error('Failed to fetch player scores for week');
   }
   const data = await response.json();
-  await client.set(`player-scores-${playerId}-${week}`, JSON.stringify(data), { EX: 300 });
   return data;
 } 
 
 export async function fetchSeasonOverviewData(): Promise<any>{
 
   const url = getApiUrl(`/api/season`);
-  const cachedData = await client.get('season');
-  if (cachedData) {
-    return JSON.parse(cachedData);
-  }
   const response = await fetch(url, {
     next: {revalidate: 0},
     cache: 'no-store'
@@ -160,17 +124,12 @@ export async function fetchSeasonOverviewData(): Promise<any>{
     throw new Error('Failed to fetch season data');
   }
   const data = await response.json();
-  await client.set('season', JSON.stringify(data), { EX: 300 });
   return data;
 }
 
 export async function fetchESPNGolfScores(): Promise<any> {
   
   const url = 'https://site.api.espn.com/apis/site/v2/sports/golf/pga/scoreboard';
-  const cachedData = await client.get('espn-golf-scores');
-  if (cachedData) {
-    return JSON.parse(cachedData);
-  }
   const response = await fetch(url, {
     next: {revalidate: 0},
     cache: 'no-store'
@@ -180,16 +139,11 @@ export async function fetchESPNGolfScores(): Promise<any> {
     throw new Error('Failed to fetch espn golf data');
   }
   const data = await response.json();
-  await client.set('espn-golf-scores', JSON.stringify(data), { EX: 300 });
   return data;
 }
 
 export async function fetchPlayerWinnings(playerId: number): Promise<any> {
   const url = getApiUrl(`/api/players/${playerId}/winnings`);
-  const cachedData = await client.get(`player-winnings-${playerId}`);
-  if (cachedData) {
-    return JSON.parse(cachedData);
-  }
   const response = await fetch(url, {
     next: {revalidate: 0},
     cache: 'no-store'
@@ -199,7 +153,6 @@ export async function fetchPlayerWinnings(playerId: number): Promise<any> {
     throw new Error('Failed to fetch player winnings');
   }
   const data = await response.json();
-  await client.set(`player-winnings-${playerId}`, JSON.stringify(data), { EX: 300 });
   return data;
 }
 
