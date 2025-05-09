3575617158c7e806488778abe513295bdd88f4ad
Jason <jbjbro6@gmail.com>
2025-05-08 23:48:12 -0400

    add back in config file for images

---
+++ next.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'standalone',
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+    compiler: {
+      removeConsole: process.env.NODE_ENV === 'production',
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'a.espncdn.com',
+          port: '',
+          pathname: '**',
+          search: '',
+        },
+      ], 
+    }
+  }
+  
+  module.exports = nextConfig 
