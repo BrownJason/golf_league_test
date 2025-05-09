321f0cdd116d9593ee692b2576acf4081c16b1d7
Jason <jbjbro6@gmail.com>
2025-05-08 22:57:22 -0400

    Change hex color to oklch:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default async function Home() {
             <Link href="/players" className="group">
               <div className="bg-[var(--card)] hover:bg-[var(--primary-hover)]/60 border border-[var(--border)] rounded-xl p-6 shadow shadow-black shadow-lg transition-all duration-300 hover:shadow-xl hover:scale-105 hover:shadow-xl">
                 <div className="w-12 h-12 rounded-full bg-[var(--card-foreground)] border-2 border-[var(--border)] flex items-center justify-center mb-4">
-                  <svg className="w-6 h-6 text-[var(--primary-foreground)]" fill="none" stroke="var(--text)" viewBox="0 0 24 24">
+                  <svg className="w-6 h-6 text-[var(--card-foreground)]" fill="none" stroke="var(--text)" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
                   </svg>
                   </div>
@@ -42,7 +42,7 @@ export default async function Home() {
             <Link href="/weekly_score" className="group">
               <div className="bg-[var(--card)] dark:bg-[var(--card)] hover:bg-[var(--primary-hover)]/60 border border-[var(--border)] rounded-xl p-6 shadow shadow-black shadow-lg transition-all duration-300 hover:shadow-xl hover:scale-105 hover:shadow-xl">
                 <div className="w-12 h-12 rounded-full bg-[var(--card-foreground)] border-2 border-[var(--border)] flex items-center justify-center mb-4">
-                  <svg className="w-6 h-6 text-[var(--primary-foreground)]" fill="none" stroke="var(--text)" viewBox="0 0 24 24">
+                  <svg className="w-6 h-6 text-[var(--card-foreground)]" fill="none" stroke="var(--text)" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
                   </svg>
                 </div>
@@ -55,7 +55,7 @@ export default async function Home() {
             <Link href="/weekly_score" className="group">
               <div className="bg-[var(--card)] hover:bg-[var(--primary-hover)]/60 border border-[var(--border)] rounded-xl p-6 shadow shadow-black shadow-lg transition-all duration-300 hover:shadow-xl hover:scale-105 hover:shadow-xl">
                 <div className="w-12 h-12 rounded-full bg-[var(--card-foreground)] border-2 border-[var(--border)] flex items-center justify-center mb-4">
-                  <svg className="w-6 h-6 text-[var(--primary)]" fill="none" stroke="var(--text)" viewBox="0 0 24 24">
+                  <svg className="w-6 h-6 text-[var(--card-foreground)]" fill="none" stroke="var(--text)" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                   </svg>
                 </div>
