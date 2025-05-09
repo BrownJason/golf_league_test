794c88de831c6eaf3b3cfbf0eaf64c9ae452feab
Jason <jbjbro6@gmail.com>
2025-05-08 23:53:17 -0400

    Fix hover on card on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default async function Home() {
 
             {/* Weekly Scores Card */}
             <Link href="/weekly_score" className="group">
-              <div className="bg-[#292929] dark:bg-[#292929] hover:bg-[#305D3C]/60 border border-[#B2825E] rounded-xl p-6 shadow shadow-black shadow-lg transition-all duration-300 hover:shadow-xl hover:scale-105 hover:shadow-xl">
+              <div className="bg-[#292929] hover:bg-[#305D3C]/60 border border-[#B2825E] rounded-xl p-6 shadow shadow-black shadow-lg transition-all duration-300 hover:shadow-xl hover:scale-105 hover:shadow-xl">
                 <div className="w-12 h-12 rounded-full bg-[#305D3C] border-2 border-[#B2825E] flex items-center justify-center mb-4">
                   <svg className="w-6 h-6 text-[#305D3C]" fill="none" stroke="#EDE6D6" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
