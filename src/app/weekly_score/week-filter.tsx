7e76171f219cb1ad4cc0ed37445df1f3493b4cf8
Jason <jbjbro6@gmail.com>
2025-05-19 21:11:31 -0400

    Set default week to correct date

--- src/app/weekly_score/week-filter.tsx
+++ src/app/weekly_score/week-filter.tsx
@@ -16,20 +16,20 @@ export default function WeekFilter({ weeks, selectedWeek, onChange }: WeekFilter
     if (!weeks || weeks.length === 0) {
         return <div className="text-[#B2825E]">No weeks available</div>;
     }
-    const maxDate = weeks.reduce((max, week) => {
-        const weekDate = new Date(week.week_date);
-        return weekDate > max ? weekDate : max;
-    }, new Date(0));
-    const formattedMaxDate = maxDate.toLocaleDateString('en-US', {
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-    });
+    // Find the week with the latest date
+    const latestWeek = weeks.reduce((latest, week) => {
+        return new Date(week.week_date) > new Date(latest.week_date) ? week : latest;
+    }, weeks[0]);
+    let week_date = "";
+    if (latestWeek && latestWeek.week_date) {
+        const [year, month, day] = latestWeek.week_date.split("-");
+        week_date = `${parseInt(month, 10)}/${parseInt(day, 10)}/${year}`;
+    }
   return (
-    <Select value={selectedWeek || formattedMaxDate} onValueChange={onChange}>
+    <Select value={selectedWeek || week_date} onValueChange={onChange}>
       <SelectTrigger className="bg-[#1A3E2A] border-[#B2825E] text-[#B2825E] w-48">
         <SelectValue placeholder="Select Week">
-            {selectedWeek ? weeks.find(week => week.week_date === selectedWeek)?.formatted_date : formattedMaxDate}
+            {selectedWeek ? weeks.find(week => week.week_date === selectedWeek)?.formatted_date : week_date}
         </SelectValue>
       </SelectTrigger>
       <SelectContent className="bg-[#1A3E2A] border-[#B2825E]">
@@ -37,7 +37,6 @@ export default function WeekFilter({ weeks, selectedWeek, onChange }: WeekFilter
           <SelectItem 
           key={week.week_date} 
           value={week.week_date} 
-          defaultValue={formattedMaxDate}
           className="text-[#B2825E]">
             {week.formatted_date}
           </SelectItem>
