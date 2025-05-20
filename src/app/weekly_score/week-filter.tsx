14a4354c60a07d7c94e0c91f910bc164d7964745
Jason <jbjbro6@gmail.com>
2025-05-19 20:59:12 -0400

    Set default week to max week on weekly scores page

--- src/app/weekly_score/week-filter.tsx
+++ src/app/weekly_score/week-filter.tsx
@@ -12,14 +12,33 @@ interface WeekFilterProps {
 }
 
 export default function WeekFilter({ weeks, selectedWeek, onChange }: WeekFilterProps) {
+    console.log(weeks)
+    if (!weeks || weeks.length === 0) {
+        return <div className="text-[#B2825E]">No weeks available</div>;
+    }
+    const maxDate = weeks.reduce((max, week) => {
+        const weekDate = new Date(week.week_date);
+        return weekDate > max ? weekDate : max;
+    }, new Date(0));
+    const formattedMaxDate = maxDate.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+    });
   return (
-    <Select value={selectedWeek || ''} onValueChange={onChange}>
+    <Select value={selectedWeek || formattedMaxDate} onValueChange={onChange}>
       <SelectTrigger className="bg-[#1A3E2A] border-[#B2825E] text-[#B2825E] w-48">
-        <SelectValue placeholder="Select Week" />
+        <SelectValue placeholder="Select Week">
+            {selectedWeek ? weeks.find(week => week.week_date === selectedWeek)?.formatted_date : formattedMaxDate}
+        </SelectValue>
       </SelectTrigger>
       <SelectContent className="bg-[#1A3E2A] border-[#B2825E]">
         {weeks.map((week) => (
-          <SelectItem key={week.week_date} value={week.week_date} className="text-[#B2825E]">
+          <SelectItem 
+          key={week.week_date} 
+          value={week.week_date} 
+          defaultValue={formattedMaxDate}
+          className="text-[#B2825E]">
             {week.formatted_date}
           </SelectItem>
         ))}
